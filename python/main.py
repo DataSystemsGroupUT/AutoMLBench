a282ff7189972730a0eacd470005bb3ad2dc3a5b
Oleh Matsuk <olehmatsuk@gmail.com>
2019-03-24 13:01:57 +0200

    Added MLPlan, started with Recipe

--- python/main.py
+++ python/main.py
@@ -1,19 +1,24 @@
 import argparse
 import warnings
-from benchmark import AutoSklearnBenchmark, TPOTBenchmark
+from typing import List
+
+from benchmark import AutoSklearnBenchmark, TPOTBenchmark, RecipeBenchmark
 
 warnings.simplefilter("ignore")
 
 
 def benchmark(dataset_file: str, output_file: str,
-              time: int, model: str, dataset_test_file: str = None):
+              time: int, model: str, dataset_test_file: str = None,
+              config: List[str] = None):
     model_to_bench = {
         'autosklearn': AutoSklearnBenchmark,
         'tpot': TPOTBenchmark,
+        'recipe': RecipeBenchmark
     }
     if model in model_to_bench:
         model_to_bench[model]().benchmark(dataset_file, output_file,
-                                          time_limit=time, dataset_test_file=dataset_test_file)
+                                          time_limit=time, dataset_test_file=dataset_test_file,
+                                          config=config)
 
 
 if __name__ == '__main__':
@@ -23,6 +28,9 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--time', type=int, help='Time budget')
     parser.add_argument('-m', '--model', help='AutoML Model')
     parser.add_argument('-te', '--test_file', help='Dataset test file')
+    parser.add_argument('-c', '--config', nargs='*')
     args = parser.parse_args()
 
-    benchmark(args.input_file, args.output_file, args.time, args.model, args.test_file)
+    benchmark(dataset_file=args.input_file, output_file=args.output_file,
+              time=args.time, model=args.model, dataset_test_file=args.test_file,
+              config=args.config)
